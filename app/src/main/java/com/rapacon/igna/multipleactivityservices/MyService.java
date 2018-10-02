f398c948dcbd8b9bbf228d580ff76376b81b8871
Igna Rapacon <2015081257@ust-ics.mygbiz.com>
2018-10-02 15:30:50 +0800

    Initial commit

---
+++ app/src/main/java/com/rapacon/igna/multipleactivityservices/MyService.java
@@ -0,0 +1,23 @@
+package com.rapacon.igna.multipleactivityservices;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class MyService extends IntentService {
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     */
+
+    public MyService() {
+        super("MyService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d(" 4itg", "Service is Running...");
+    }
+}
